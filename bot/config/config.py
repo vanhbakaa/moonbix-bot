74cc2f2e342fd1e80cd8aa9afb221c2bfba302dc
Vanhbaka <vanhbaka2k7@gmail.com>
2024-09-21 23:43:41 +0700

    Update config.py

--- bot/config/config.py
+++ bot/config/config.py
@@ -10,7 +10,7 @@ class Settings(BaseSettings):
 
     REF_LINK: str = "https://t.me/Binance_Moonbix_bot/start?startApp=ref_6624523270&startapp=ref_6624523270&utm_medium=web_share_copy"
     AUTO_TASK: bool = True
-    AUTO_PLAY_GAME: bool = False # please don't enable it i still working on it!
+    AUTO_PLAY_GAME: bool = True
 
     USE_PROXY_FROM_FILE: bool = False
 
