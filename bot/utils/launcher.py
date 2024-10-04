75672658d05cb7b5fab1deebfdaf8de3309f2dda
Vanhbaka <vanhbaka2k7@gmail.com>
2024-10-04 22:00:44 +0700

    Update launcher.py

--- bot/utils/launcher.py
+++ bot/utils/launcher.py
@@ -17,7 +17,7 @@ from bot.core.registrator import register_sessions
 
 import importlib.util
 
-curr_version = "2.7.0"
+curr_version = "2.7.1"
 
 version = requests.get("https://raw.githubusercontent.com/vanhbakaa/moonbix-bot/refs/heads/main/version")
 version_ = version.text.strip()
